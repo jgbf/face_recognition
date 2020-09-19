40003d46b08909b705b8cc8e38de5c335eb85340
jgbf <mark.kelemen@antavo.com>
2020-09-20 00:21:17 +0200

    Change scale for better framerate

--- src/server.py
+++ src/server.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route("/video_feed")
 def video_feed():
-    return Response(cam.stream(), mimetype="multipart/x-mixed-replace; boundary=frame")
+    return Response(cam.stream(scale=0.25), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', port=5000)
